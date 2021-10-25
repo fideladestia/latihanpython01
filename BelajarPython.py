75a0ce2c71fd49c7f54a3b1b8be174479a963abe
User <fideladestia>
2021-10-25 15:14:15 +0700

    menambahkan file

--- BelajarPython.py
+++ BelajarPython.py
@@ -1,2 +1,10 @@
-print("hello")
-print("Hai Saya Sedang Belajar Python")
+nama=input("siapa nama anda:")
+namap=input("biasa dipanggil:")
+nim=input("berapa no nim anda:")
+ttl=input("berapakah umur anda:")
+umur=input("berapakah umur anda:")
+alamat=input("dimana anda tinggal:")
+no=input("berapa no telepon anda:")
+print("siapa nama anda",nama,"biasa dipanggil",namap,"berapa no nim anda",nim,
+"dimana dan kapan anda lahit",ttl,"berapa umur anda",umur,"dimana anda tinggal",alamat,
+"berapa no telepon anda",no,)
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # latihanpython01
+## belajar input dan output data pada python
+### cara menginput pada python
+<p> contoh nama=input("siapa nama kamu")
+
+### contoh gambar output data
+![gambar1](ss/ssoutput.png)
